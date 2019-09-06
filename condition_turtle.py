d563207999748778347b7eee96169587aef77e60
Jared Amalong <amalong@gmail.com>
2019-09-06 13:34:43 -0700

    Adding turtle preview files

---
+++ condition_turtle.py
@@ -0,0 +1,22 @@
+import turtle
+wn = turtle.Screen()
+
+amy = turtle.Turtle()
+amy.pencolor("Pink")
+amy.forward(50)
+if amy.pencolor() == "Pink":
+    amy.right(60)
+    amy.forward(100)
+else:
+    amy.left(60)
+    amy.forward(100)
+
+kenji = turtle.Turtle()
+kenji.pencolor("Green")
+kenji.forward(60)
+if kenji.pencolor() == "Pink":
+    kenji.right(60)
+    kenji.forward(100)
+else:
+    kenji.left(60)
+    kenji.forward(100)
